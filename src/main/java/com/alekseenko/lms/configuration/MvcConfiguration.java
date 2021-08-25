27123f9e16f76e190a4483b65feebd533083d2a3
preposterone <ivan38rus@gmail.com>
2021-08-25 18:34:41 +0300

    Initial implementation of email sender

---
+++ src/main/java/com/alekseenko/lms/configuration/MvcConfiguration.java
@@ -0,0 +1,23 @@
+package com.alekseenko.lms.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class MvcConfiguration implements WebMvcConfigurer {
+
+  @Bean
+  public MessageSource messageSource() {
+    ReloadableResourceBundleMessageSource messageSource
+        = new ReloadableResourceBundleMessageSource();
+
+    messageSource.setBasename("classpath:messages");
+    messageSource.setDefaultEncoding("UTF-8");
+    return messageSource;
+  }
+}
